596c0db6d025f786d5332b5e73fde7ac4337856f
Vinay Reddy <oppurivinay25@gmail.com>
2025-09-17 14:47:43 +0530

    test-1

--- components/layout/bottom-nav.tsx
+++ components/layout/bottom-nav.tsx
@@ -4,16 +4,16 @@ import Link from "next/link"
 const BottomNav = () => {
     return (
         <nav className="fixed bottom-2 left-2 right-2  z-50 md:hidden rounded-full border backdrop-blur-md bg-background/70 px-3">
-            <div className="flex h-14 justify-between items-center">
+            <div className="flex justify-between items-center">
                 {BottomNavLinks.map(({ href, icon: Icon, label: Label }) => {
                     return (
                         <Link
                             key={href}
                             href={href}
-                            className="flex flex-col items-center justify-center text-xs text-muted-foreground gap-1 px-4 py-3"
+                            className="flex flex-col items-center justify-center text-xs text-muted-foreground gap-1.5 px-4 py-2.5"
                         >
                             <div className='flex items-center justify-center rounded-full'>
-                                <Icon className="size-4" />
+                                <Icon className="size-4.5" />
                             </div>
                             <div className='flex items-center justify-center rounded-full' >
                                 {Label}
